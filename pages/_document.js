7eee745ae2b771d52bbae6fc068a22f35cded0d4
Jayden <chel8780@gmail.com>
2023-04-22 15:59:36 +0900

    init

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Link from 'next/link'
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+      <div className="navbar">
+        <Link href="/">홈</Link>
+        <Link href="/list">List</Link>
+      </div>
         <Main />
         <NextScript />
       </body>
