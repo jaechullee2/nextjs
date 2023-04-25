5e62054dee10bb1ed7b5b15cc0b655116b67acd9
Jayden <chel8780@gmail.com>
2023-04-25 16:58:35 +0900

    cart Upload

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,5 +1,7 @@
+
 export default function List(){
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    
     // let 어레이 = [2,3,4]
     // let b = 어레이.map((a)=>{
     //     return 10
@@ -12,7 +14,8 @@ export default function List(){
                 상품.map((n, i)=>{
                     return (
                         <div className="food" key={i}>
-                            <h4>{상품[i]} $40</h4>
+                            <img src={`/food${i}.jpeg`} className="food-img" alt="food"/>
+                            <h4>{n} $40</h4>
                         </div>
                     )
                 })
