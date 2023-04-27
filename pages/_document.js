a011b4461b98572f43f054e8fa535f816e8554a2
Jayden <chel8780@gmail.com>
2023-04-27 16:01:57 +0900

    head bar Update

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
       <div className="navbar">
         <Link href="/">홈</Link>
         <Link href="/list">List</Link>
+        <Link href="/cart">Cart</Link>
       </div>
         <Main />
         <NextScript />
