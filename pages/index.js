9abc96873e8dcd5a0fc198bc05277b003d03e072
Jayden <chel8780@gmail.com>
2023-05-02 23:52:11 +0900

    home Update

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <h4 className="title" style={{ color : 'red', fontSize:'30px'
     }}>애플후레시</h4>
       <p className="title-sub">by dev {name} {age}</p>
-      <a href={link}>링크</a>
+      <a href={ link }>링크</a>
     </div>
   )
 }
